6d06445cafc8e60c28f70091ebea19a6a5eadc28
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-02-23 15:02:25 +0100

    Temporarily removed google auth

--- app/(auth)/auth-client.tsx
+++ app/(auth)/auth-client.tsx
@@ -138,10 +138,16 @@ export default function AuthClientPage({
         {/* Social Auth */}
         <button
           onClick={() => handleSocialAuth("google")}
-          disabled={isLoading}
-          className="w-full flex items-center justify-center gap-3 bg-[#0F1623] hover:bg-white/5 hover:cursor-pointer border border-white/10 rounded-xl py-3 text-sm transition"
+          disabled={true}
+          className="relative w-full flex items-center justify-center gap-3 bg-[#0F1623] border border-white/10 rounded-xl py-3 text-sm opacity-50 cursor-not-allowed"
         >
-          <svg className="w-5 h-5" viewBox="0 0 24 24">
+          {/* Coming Soon badge */}
+          <span className="absolute top-0.5 sm:top-2 left-1 sm:left-3 text-[10px] px-2 py-0.5 bg-indigo-600/20 text-indigo-400 border border-indigo-500/30 rounded-full leading-none">
+            Coming Soon
+          </span>
+
+          {/* Google Logo */}
+          <svg className="w-5 h-5 shrink-0" viewBox="0 0 24 24">
             <path
               fill="#4285F4"
               d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
@@ -159,7 +165,11 @@ export default function AuthClientPage({
               d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
             />
           </svg>
-          Continue with Google
+
+          {/* Button Text */}
+          <span className="whitespace-nowrap">
+            Continue with Google
+          </span>
         </button>
 
         {/* Divider */}
